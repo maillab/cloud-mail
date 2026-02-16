a03566977a7d631d50fadda145565f95d83181e2
abyn365 <abyanakhtar354@gmail.com>
2026-02-16 15:33:33 +0000

    testf

--- mail-vue/src/i18n/en.js
+++ mail-vue/src/i18n/en.js
@@ -125,7 +125,7 @@ const en = {
     loginBtn: 'Sign in',
     regBtn: 'Sign up',
     loginTitle: 'Sign in to your account to access email',
-    regTitle: 'Sign up for an account to use email',
+    regTitle: "Need access? Email admin@abyn.xyz to obtain an account or invite code.",
     confirmPwd: 'Confirm password',
     regKeyOptional: 'Invite code (Optional)',
     noAccount: 'Don\'t have an account? \u200B',
